c72058827d229027b570ba5034dc2122ad002681
DESKTOP-KIEUPHU\PhungLe <lethikieuphung1212@gmail.com>
2018-05-09 04:19:34 +0700

    thêm setting để chọn mức độ bài học

--- app/src/main/java/com/example/phungle/finalproject/Sentence.java
+++ app/src/main/java/com/example/phungle/finalproject/Sentence.java
@@ -6,11 +6,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Sentence extends AppCompatActivity {
 
     private Button btn_start;
     private Button btn_view;
+    private ImageButton btn_setting;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,6 +21,7 @@ public class Sentence extends AppCompatActivity {
 
         btn_start = (Button) findViewById(R.id.btn_start_sentence);
         btn_view = (Button) findViewById(R.id.btn_view_sentence);
+        btn_setting = (ImageButton) findViewById(R.id.btn_setting);
 
         btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +34,13 @@ public class Sentence extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Sentence.this, ViewSentence.class);
+                startActivity(intent);
+            }
+        });
+        btn_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Sentence.this, Setting.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/phungle/finalproject/StartSentence.java
+++ app/src/main/java/com/example/phungle/finalproject/StartSentence.java
@@ -1,13 +1,50 @@
 package com.example.phungle.finalproject;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import io.netopen.hotbitmapgg.library.view.RingProgressBar;
+
 public class StartSentence extends AppCompatActivity {
+
+    RingProgressBar progressBar;
+
+    int progress = 0;
+
+    Handler myHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 0)
+                if (progress < 100) {
+                    progress++;
+                    progressBar.setProgress(progress);
+                    super.handleMessage(msg);
+                }
+        }
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_sentence_layout);
+        setContentView(R.layout.start_ask_anwser_layout);
+
+        progressBar = (RingProgressBar) findViewById(R.id.processBarTime);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0 ; i < 100 ; i++){
+                    try {
+                        Thread.sleep(100);
+                        myHandler.sendEmptyMessage(0);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 }
