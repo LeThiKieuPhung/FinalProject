7f16af3cb2689c5647749cd8ec86f2b05e9f349e
lyphucloi <lyphucloi.it@gmail.com>
2018-05-20 17:45:22 +0700

    add default topic

--- app/src/main/java/com/example/phungle/finalproject/MainActivity.java
+++ app/src/main/java/com/example/phungle/finalproject/MainActivity.java
@@ -56,23 +56,32 @@ public class MainActivity extends AppCompatActivity {
         List<QuesAndAnswer> list = new ArrayList<>();
         list.add(new QuesAndAnswer("How are you ?","I'm fine"));
         list.add(new QuesAndAnswer("Good Morning","Good Morning"));
+        list.add(new QuesAndAnswer("what’s your name?","my name’s Nancy"));
+        list.add(new QuesAndAnswer("How was your day?","The same as usua"));
         GlobalData.listTopic.add(new Topic("Topic 1",list));
 
         List<QuesAndAnswer> list2 = new ArrayList<>();
         list2.add(new QuesAndAnswer("How old are you ?","I 15 year old"));
         list2.add(new QuesAndAnswer("Would you like some tea ?","Yes I do"));
+        list2.add(new QuesAndAnswer("Do you like travel ?","Yes. I love traveling"));
+        list2.add(new QuesAndAnswer("4\tWhat do you have for breakfast ?",
+                "I eat sandwich and drink milk"));
         GlobalData.listTopic.add(new Topic("Topic 2",list2));
 
         List<String> list3= new ArrayList<>();
-        list3.add("Hello");
-        list3.add("Happy");
-        list3.add("Bad");
+        list3.add("My name’s John");
+        list3.add("I’m busy now");
+        list3.add("What’s up");
+        list3.add("Don’t forget to close the door");
+        list3.add("Is there anything else? ");
         GlobalData.listTopicSent.add(new TopicSent("Topic 1",list3));
 
         List<String> list4 = new ArrayList<>();
-        list4.add("Home");
-        list4.add("Sad");
-        list4.add("Fun");
+        list4.add("Let’s have dinner");
+        list4.add("I got flu What should I do");
+        list4.add("I like spending time with my family");
+        list4.add("See you later");
+        list4.add("See you soon");
         GlobalData.listTopicSent.add(new TopicSent("Topic 2",list4));
     }
 }
